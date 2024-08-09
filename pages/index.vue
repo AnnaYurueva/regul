64ca124217a49e1a3252a9fe983352b4f6a4824f
AnnaYureva <a.yureva@worksolutions.ru>
2024-08-09 13:58:49 +0300

    стилизация выпадающего списка на главной

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,30 @@
 <template>
-   <div class="filter-container">
-      <v-select label="Select" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-      variant="solo-filled"></v-select>
-   </div>
-   <div class="cards-container mx-2 mx-md-8">
-      <GoodsServicesCard v-for="item of data" :key="item.id" :price="item.price" :title="item.title" :photo="item.photo"
-         :views="item.views" :days="item.days" :status="item.status" />
-   </div>
+   <main>
+      <div class="filter-container">
+         <v-select v-model="select" :items="items" item-title="category" item-value="category" label="Select"
+            return-object single-line variant="solo-filled">
+            <template v-slot:item="{ props, item }">
+               <v-list-item v-bind="props" :subtitle="item.raw.count"></v-list-item>
+            </template>
+         </v-select>
+      </div>
+      <div class="cards-container mx-2 mx-md-8">
+         <GoodsServicesCard v-for="item of data" :key="item.id" :price="item.price" :title="item.title" :photo="item.photo"
+            :views="item.views" :days="item.days" :status="item.status" />
+      </div>
+   </main>
 </template>
 
 <script setup>
+const select = ref({ category: 'Все товары и услуги', count: null })
+const items = [
+   { category: 'Все товары и услуги', count: null },
+   { category: 'Гробы', count: 5 },
+   { category: 'Урны', count: 12 },
+   { category: 'Тапки', count: 2 },
+   { category: 'Покрывало', count: 1 },
+]
+
 const data = [
    {
       id: 1,
@@ -86,7 +101,7 @@ const data = [
 ]
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .cards-container {
    display: grid;
 
@@ -102,5 +117,44 @@ const data = [
 .filter-container {
    max-width: 440px;
    margin: 24px auto;
+}
+
+.v-list {
+   padding: 12px 8px !important;
+   background: #f5f5f5;
+
+   .v-list-item--active {
+      .v-list-item__overlay {
+         background: none !important;
+         background-color: transparent !important;
+      }
+
+      .v-list-item__content {
+         position: relative;
+
+         &:after {
+            content: "";
+            display: block;
+            background: url('/public/icons/check.svg');
+            width: 24px;
+            height: 24px;
+            color: #337566;
+            position: absolute;
+            right: 0;
+         }
+      }
+   }
+
+   @media (max-width:960px) {
+      .v-list-item {
+         border-bottom: 1px solid #e8e8e8;
+      }
+   }
+}
+
+.v-list-item__content {
+   display: flex;
+   align-items: center;
+   gap: 8px;
 }
 </style>
